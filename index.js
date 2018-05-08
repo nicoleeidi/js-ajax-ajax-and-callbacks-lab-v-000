debe7cafa0fa71555cb29e22588dc43ade1299bf
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-08 20:10:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,3 +30,11 @@ function searchRepositories(){
 function showCommits(){
 
 }
+var renderCommit = (commit) => {
+  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+}
+
+var renderCommits = (data) => {
+  let result = data.map((commit)=>renderCommit(commit)).join('')
+  return `<ul>${result}</ul>`
+}
