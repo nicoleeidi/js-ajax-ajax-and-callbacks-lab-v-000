e3baa3b05a6a9b6884cf4685970b690222917cde
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-08 20:08:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ var searchRepositories = () => {
       displayError()
     })
 }
+var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
 function showCommits(){
 
 }
