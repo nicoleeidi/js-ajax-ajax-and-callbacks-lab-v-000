f6e341ddc2602158f932a665700ea76b6b9b1f23
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-08 18:01:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 $(document).ready(function (){
 
-  function searchRepositories(searchTerms){
-//queries the GitHub repository search API.
+
   }
 });
+function searchRepositories(searchTerms){
+
+}
+
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText)
+  const repoList = '<ul>' + repos.map(r => {
+   return (`
+          <li>
+            <h2><a href="${r.html_url}">${r.name}</a></h2>
+            <p>Watchers: ${r.watchers_count}</p>
+            <p>Forks: ${r.forks_count}</p>
+            <p>Issues: ${r.open_issues_count}</p>
+          </li>`
+          )
+  }).join('') + "</ul>"
+  document.getElementById("repositories").innerHTML = repoList
+}
