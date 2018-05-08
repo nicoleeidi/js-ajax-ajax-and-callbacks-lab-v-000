ad55298e981e9590b814aca6550658c1aebcd7ac
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-08 20:08:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ $(document).ready(function (){
 var displayError = () => $('#errors').html("I'm sorry, there's been an error. Please try again.")
 
 
-var searchRepositories = () => {
+function searchRepositories(){
   const searchTerms = $('#searchTerms').val()
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
       $('#results').html(
