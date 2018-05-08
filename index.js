0ba0a25255c0b7136e935686c271774eb0d7063a
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-08 20:00:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,11 @@ $(document).ready(function (){
 var searchRepositories = () => {
   const searchTerms = $('#searchTerms').val()
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
-      $('#results').html(renderSearchResults(data))
-    }).fail(error => {
+      $('#results').html(
+
+
+      )
+      }).fail(error => {
       displayError()
     })
 }
